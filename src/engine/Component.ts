25a7c4f892527ba508fa873e931a110bb78b79db
Jonatas <jmoraes@gmail.com>
2020-08-30 00:37:42 -0300

    some linting

--- src/engine/Component.ts
+++ src/engine/Component.ts
@@ -1,19 +1,21 @@
-let Hbs: any;
+let Hbs: Record<string, (arg0: unknown) => string>;
 
 class Component {
 
   /**
    * @param template the name of the template to be used on render
+   * @param configure method to apply changes to the DOM after the template has been mounted
    */
   constructor(
-    protected template?: string,
+    protected template: string,
+    protected configure?: (data: unknown) => void,
   ) {}
 
   /**
    * Overwrite this method to send a request to backend using params, them call mount using the response as data.
    * As a default we just bypass the params as data.
    */
-  public load(mountPoint: HTMLElement | null, params?: any, onMounted?: () => void): void {
+  public load(mountPoint: HTMLElement | null, params?: unknown, onMounted?: () => void): void {
     this.mount(mountPoint, params, onMounted);
   }
 
@@ -26,7 +28,9 @@ class Component {
       return;
     }
     mountPoint.innerHTML = this.render(data);
-    this.configure(data);
+    if (this.configure) {
+      this.configure(data);
+    }
     if (onMounted) {
       onMounted();
     }
@@ -48,9 +52,4 @@ class Component {
 
     return hbs(data);
   }
-
-  /**
-   * Apply changes to the DOM after the template has been mounted
-   */
-  public configure(data?: unknown) {}
 }
